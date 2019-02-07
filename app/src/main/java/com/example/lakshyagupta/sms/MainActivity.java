9d90c0df68d086876752c1862fcc4a188f8cf256
LAKSHYA GUPTA <lakshya.gupta7may@gmail.com>
2019-02-07 18:18:05 +0530

    first app

---
+++ app/src/main/java/com/example/lakshyagupta/sms/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.lakshyagupta.sms;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mobileno,message;
+    Button sendsms;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mobileno=(EditText)findViewById(R.id.editText1);
+        message=(EditText)findViewById(R.id.editText2);
+        sendsms=(Button)findViewById(R.id.button1);
+        sendsms.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                String no=mobileno.getText().toString();
+                String msg=message.getText().toString();
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                PendingIntent pi=PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
+                SmsManager sms=SmsManager.getDefault();
+                sms.sendTextMessage(no, null, msg, pi,null);
+
+                Toast.makeText(getApplicationContext(), "Message Sent successfully!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
